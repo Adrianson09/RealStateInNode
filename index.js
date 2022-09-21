85b95ddeeaf74181f5752561a0b53f42466de14a
Adrian Guerrero <adrianson@outlook.com>
2022-09-20 18:21:07 -0600

    First Commit, base of node app

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const res = require('express/lib/response');
+
+// Crear la app
+
+const app = express()
+
+// routing
+app.get('/', function(req, res) {
+    res.send('Hola Mundo en express')
+});
+app.get('/nosotros', function(req, res) {
+    res.send('Información de Nosotros')
+});
+
+// definir un puerto y arrancar el proyectot
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`EL servidor está funcionando en el puerto ${port}`)
+});
