acacc6f581108cd4bb34823a04e9bafa066181fd
Adrian Guerrero <adrianson@outlook.com>
2022-09-22 10:46:17 -0600

    add error msg pag registro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from 'express-validator'
 import Usuario from '../models/Usuario.js'
 
 const formularioLogin = (req, res) => {
@@ -12,6 +13,27 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+// validation 
+    await check('nombre').notEmpty().withMessage('El nombre es Obligatorio').run(req)
+    await check('email').isEmail().withMessage('Eso no parece un Email').run(req)
+    await check('password').isLength({ min: 8 }).withMessage('El password debe ser de al menos 8 caracteres').run(req)
+    await check('repetir-password').equals('password').withMessage('Los passwords no son iguales').run(req)
+
+
+    let resultado = validationResult(req)
+
+    // return res.json(resultado.array())
+
+    // Verificar el resultado
+
+    if (!resultado.isEmpty()) {
+        // Errores
+        return res.render('auth/registro', {
+            pagina: 'Crear Cuenta',
+            errores: resultado.array()
+        })
+    }
+
 
     const usuario = await Usuario.create(req.body);
 
