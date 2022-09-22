c1028c45190cb67eeb3724a75483e78290077ca0
Adrian Guerrero <adrianson@outlook.com>
2022-09-21 18:32:42 -0600

    seventh commit add forms login, forgot password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,15 +1,21 @@
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
-        autenticado: false
+        pagina: 'Iniciar Sesión'
     })
 }
 const formularioRegistro = (req, res) => {
     res.render('auth/registro', {
-
+        pagina: 'Crear Cuenta'
+    })
+}
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/olvide-password', {
+        pagina: 'Recuperar acceso a Caribe Hospitality'
     })
 }
 
 export {
     formularioLogin, 
-    formularioRegistro
+    formularioRegistro,
+    formularioOlvidePassword
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { formularioLogin, formularioRegistro } from '../controllers/usuarioController.js';
+import { formularioLogin, formularioRegistro, formularioOlvidePassword } from '../controllers/usuarioController.js';
 const router = express.Router();
 
 router.get('/login', formularioLogin );
 router.get('/registro', formularioRegistro );
+router.get('/olvide-password', formularioOlvidePassword );
 
 
 export default router
