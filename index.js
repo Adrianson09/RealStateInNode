3518409de6cb60daff9fbd5ae9e79375f79df1d1
Adrian Guerrero <adrianson@outlook.com>
2022-09-21 21:29:08 -0600

    eight commit db conector

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from 'express'
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import db from './config/db.js'
 // Crear la app
 const app = express()
+
+// Conexión a la base de datos
+try {
+    await db.authenticate();
+    console.log('Conexión correcta a la BD ')
+} catch (error) {
+    console.log(error)
+}
 // habilitar pug
 app.set('view engine', 'pug')
 app.set('views', './views')
